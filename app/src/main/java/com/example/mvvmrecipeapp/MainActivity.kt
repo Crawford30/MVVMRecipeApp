8e04f9e64318847188e007fd89942ac1e5949798
Joel Crawford <joelcrawford30@gmail.com>
2023-12-08 19:11:09 +0300

    Change orientation in the column

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.mvvmrecipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -27,55 +25,90 @@ import com.example.mvvmrecipeapp.ui.theme.MVVMRecipeAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Gray)
-                    .verticalScroll(
-                        rememberScrollState(),
-                    )
-            ) {
-
-                Image(
-                    painter = painterResource(id = R.drawable.happy_meal_small),
-                    contentDescription = "",
+            Column {
+                Column(
                     modifier = Modifier
-                        .height(300.dp)
-                        .fillMaxWidth(),
-                    contentScale = ContentScale.Crop,
-                )
+                        .fillMaxWidth()
+                        .background(Color.Gray)
+                        .height(200.dp)
+                        .border(BorderStroke(1.dp, Color.Red)),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        "Happy Coding",
+                        Modifier.align(Alignment.CenterHorizontally)
+                    )
+                }
+                Spacer(modifier = Modifier.padding(20.dp))
 
-                Column(modifier = Modifier.padding(16.dp)) {
+                Row(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .background(Color.Gray)
+                        .height(200.dp)
+                        .border(BorderStroke(1.dp, Color.Red))
+                ) {
                     Text(
-                        text = "Happy Meal",
-                        style = TextStyle(
-                            fontSize = 26.sp
-                        )
+                        "Happy Coding",
+                        Modifier.align(Alignment.CenterVertically)
                     )
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-
-                    Text(
-                        text = "800 Calories",
-                        style = TextStyle(
-                            fontSize = 17.sp
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(
-                        text = "$5.9",
-                        color = Color(0xFF85bb65),
-                        style = TextStyle(
-                            fontSize = 15.sp
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
 
                 }
+
             }
 
-
         }
+//        setContent {
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .background(Color.Gray)
+//                    .verticalScroll(
+//                        rememberScrollState(),
+//                    )
+//            ) {
+//
+//                Image(
+//                    painter = painterResource(id = R.drawable.happy_meal_small),
+//                    contentDescription = "",
+//                    modifier = Modifier
+//                        .height(300.dp)
+//                        .fillMaxWidth(),
+//                    contentScale = ContentScale.Crop,
+//                )
+//
+//                Column(modifier = Modifier.padding(16.dp)) {
+//                    Text(
+//                        text = "Happy Meal",
+//                        style = TextStyle(
+//                            fontSize = 26.sp
+//                        )
+//                    )
+//                    Spacer(modifier = Modifier.padding(top = 10.dp))
+//
+//                    Text(
+//                        text = "800 Calories",
+//                        style = TextStyle(
+//                            fontSize = 17.sp
+//                        )
+//                    )
+//                    Spacer(modifier = Modifier.padding(top = 10.dp))
+//                    Text(
+//                        text = "$5.9",
+//                        color = Color(0xFF85bb65),
+//                        style = TextStyle(
+//                            fontSize = 15.sp
+//                        )
+//                    )
+//                    Spacer(modifier = Modifier.padding(top = 10.dp))
+//
+//                }
+//            }
+//
+//
+//        }
 //            MVVMRecipeAppTheme {
 //                // A surface container using the 'background' color from the theme
 //                Surface(
