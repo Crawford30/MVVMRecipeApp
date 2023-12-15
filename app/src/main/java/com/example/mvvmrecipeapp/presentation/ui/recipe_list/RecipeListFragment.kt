d0cd741cbdf89517db57bccaa290b2dd20e14e03
Joel Crawford <joelcrawford30@gmail.com>
2023-12-15 16:22:17 +0300

    Input capturing for input field

--- app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -13,6 +13,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
@@ -51,15 +55,32 @@ class RecipeListFragment : Fragment() {
                  *The fragment change a bit since we're using MutableState
                  * We get the value inside composables
                  */
-                val recipes = viewModel.recipes.value //anytime the list, the value changes and any composable using this value will also change
+                val recipes =
+                    viewModel.recipes.value //anytime the list, the value changes and any composable using this value will also change
 
-                LazyColumn(
-
-                ){
-                    itemsIndexed(
-                        items = recipes
-                    ){index, item ->  
-                        RecipeCard(recipes = item, onClick = {})
+                /**
+                 *mutable data structure to get value from the input field
+                 * remember mentioned in JS doc, it uses the remember to store an instance of the object in memory
+                 * remember can store both mutable or immutable object
+                 * In fragment its private val query = ""
+                 */
+                val query = remember {
+                    mutableStateOf("beef")
+                }
+                Column() {
+                    TextField(
+                        value = query.value,
+                        onValueChange = { newValue ->
+                            query.value = newValue
+                        }
+                    )
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    LazyColumn() {
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, item ->
+                            RecipeCard(recipes = item, onClick = {})
+                        }
                     }
                 }
 
