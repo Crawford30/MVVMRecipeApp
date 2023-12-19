4677a8242f2a82ff0e03587bbb342f2eda981f44
Joel Crawford <joelcrawford30@gmail.com>
2023-12-19 13:09:15 +0300

    animation

---
+++ app/src/main/java/com/example/mvvmrecipeapp/presentation/components/PulsingDemo.kt
@@ -0,0 +1,14 @@
+package com.example.mvvmrecipeapp.presentation.components
+
+import android.util.FloatProperty
+import androidx.compose.runtime.Composable
+
+
+//Singleton object
+object PulseAnimationDefinitions {
+
+    enum class PulseState {
+        INITIAL, FINAL
+    }
+ 
+}
