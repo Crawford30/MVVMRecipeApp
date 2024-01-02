786122180b91a75265c51cf7137d5747caa0baa1
Joel Crawford <joelcrawford30@gmail.com>
2024-01-02 12:08:19 +0300

    Font change

--- app/src/main/java/com/example/mvvmrecipeapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/ui/theme/Type.kt
@@ -10,13 +10,101 @@ import androidx.compose.ui.unit.sp
 import com.example.mvvmrecipeapp.R
 
 
+
+
+private val BookmanOldStyle = FontFamily(
+    Font(R.font.bookman_old_style_normal, FontWeight.W300),
+    Font(R.font.bookman_old_style_regular, FontWeight.W400),
+    Font(R.font.bookman_old_style_italic, FontWeight.W500),
+    Font(R.font.bookman_old_style_bold_italic, FontWeight.W600)
+)
+
+
+val BookmanOldStyleTypographyObj = Typography(
+    h1 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W500,
+        fontSize = 30.sp
+    ),
+
+    h2 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W500,
+        fontSize = 24.sp
+    ),
+
+    h3 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W500,
+        fontSize = 20.sp
+    ),
+
+    h4 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W400,
+        fontSize = 18.sp
+    ),
+
+    h5 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp
+    ),
+
+    h6 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp
+    ),
+
+    subtitle1 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W400,
+        fontSize = 15.sp,
+        color = Color.White
+    ),
+    caption = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    overline = TextStyle(
+        fontFamily = BookmanOldStyle,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp
+    )
+
+
+)
+
+
+
+
 private val Quicksand = FontFamily(
     Font(R.font.quicksand_light, FontWeight.W300),
     Font(R.font.quicksand_regular, FontWeight.W400),
     Font(R.font.quicksand_medium, FontWeight.W500),
     Font(R.font.quicksand_bold, FontWeight.W600)
 )
-
 
 val QuickSandTypographyObj = Typography(
     h1 = TextStyle(
